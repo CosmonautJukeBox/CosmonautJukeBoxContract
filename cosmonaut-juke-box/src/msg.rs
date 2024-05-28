067ebc22bf26a96d486409d8f48205a63c25d83f
Sofiia <sofiiavarich2@gmail.com>
2024-05-28 19:13:41 +0200

    Created functions for LED: -ChangeLEDs -ResetLEDs -PlaySound

--- cosmonaut-juke-box/src/msg.rs
+++ cosmonaut-juke-box/src/msg.rs
@@ -4,8 +4,26 @@ use cosmwasm_schema::{cw_serde, QueryResponses};
 pub struct InstantiateMsg {}
 
 #[cw_serde]
-pub enum ExecuteMsg {}
+pub enum ExecuteMsg {
+    #[serde(rename = "change_leds")]
+    ChangeLEDs { red: u8, green: u8,blue: u8 },
+    #[serde(rename = "reset_leds")]
+    ResetLEDs,
+    PlaySound { index: usize }
+}
 
 #[cw_serde]
 #[derive(QueryResponses)]
-pub enum QueryMsg {}
+pub enum QueryMsg {
+    #[returns(LEDResponse)]
+    #[serde(rename = "get_leds")]
+    GetLEDs {}
+}
+
+#[cw_serde]
+#[serde(rename = "led_response")]
+pub struct LEDResponse {
+    pub red: u8,
+    pub green: u8,
+    pub blue: u8
+}
